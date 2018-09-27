82b010e19ad267ee4a8e2fdfca77d6ebe87be162
Phil <Phil@LAPTOP-1FJ4H7FA.edu.ds.fhnw.ch>
2018-09-27 10:36:10 +0200

    added files

---
+++ connectdb.php
@@ -0,0 +1,14 @@
+<?php
+    $dbHost = "127.0.0.1";		//Location Of Database
+    $dbUser = "root";			//Database User Name 
+    $dbPass = "";				//Database Password 
+    $dbDatabase = "test2";		//Database Name
+
+	// Create connection
+	$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbDatabase);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+?>
---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="all.css">
+	</head>
+	<body>
+		<div id="login_box">
+			<form action="access.php" method="post">
+				<input type="email" id="textbox_username" class="textboxes" name="username" placeholder="username" required autofocus></br>
+				<input type = "password" id="textbox_password" class="textboxes" name="password" placeholder="password" required><br>
+				<a id="link_pwvergessen" href="#" onclick="doSomething();">Forgot password?</a>
+				<button type="submit" id="btn_login" name="login">Login</button>
+			</form>
+		</div>
+	</body>
+</html>
