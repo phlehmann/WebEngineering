f2cca7b374c79d88160474e73b16e868027d93a3
bodog <bodog@DESKTOP-39D644H.edu.ds.fhnw.ch>
2018-10-15 09:46:50 +0200

    Merge origin/master
    Conflicts:
    	index.php

--- userprofile.php
+++ userprofile.php
@@ -8,18 +8,26 @@ author: Bodo Grütter
         <link rel="stylesheet" type="text/css" href="userprofile.css">
         <link rel="stylesheet" type="text/css" href="all.css">
         <title>Benutzerkonto</title>
+        <script type="text/javascript">
+            function edit(id){
+                
+            }
+        </script>
     </head>
     <body>
         <?php
         // put your code here
         ?>
+        <h3>Benutzerprofil</h3>
+        <table>
         <form name="record" action="userprofile.php">
-            Name<input type="text" name="name" value="" /><br/>
-            Strasse<input type="text" name="strasse" value="" /><br/>
-            PLZ<input type="text" name="plz" value="" /><br/>
-            Ort<input type="text" name="ort" value="" /><br/>
-            <input type="submit" value="Speichern" name="save" />
-            <input type="reset" value="Zurücksetzen" name="save" />
+            <tr><td>Name</td><td><input type="text" name="name" value="" /></td></tr>
+            <tr><td>Strasse</td><td><input type="text" name="strasse" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
+            <tr><td>PLZ</td><td><input type="text" name="plz" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
+            <tr><td>Ort</td><td><input type="text" name="ort" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
+            <tr><td><input type="submit" value="Speichern" name="save" /></td>
+            <td><input type="reset" value="Zurücksetzen" name="save" /></td></tr>
         </form>
+        </table>
     </body>
 </html>
