9c1af667f47d4c43f1aafcf3ceedc0af62e4f8b4
bodog <bodog@192.168.1.105>
2018-10-21 12:15:01 +0200

    Merge origin/master
    Conflicts:
    	index.php

--- userprofile.php
+++ userprofile.php
@@ -8,26 +8,34 @@ author: Bodo Grütter
         <link rel="stylesheet" type="text/css" href="userprofile.css">
         <link rel="stylesheet" type="text/css" href="all.css">
         <title>Benutzerkonto</title>
-        <script type="text/javascript">
-            function edit(id){
-                
-            }
-        </script>
     </head>
     <body>
         <?php
-        // put your code here
+            include("includes/DBconnection.inc.php");
+            //$myusername = $_REQUEST['email']; //Fehler?
+            
+            $query = "Select ID, Name, Strasse, Ort, Postleitzahl from bildungsinstitut where ID = 1"; //Just 4 Tests
+            //$sql = "Select ID, Name, Strasse, Ort, Postleitzahl from bildungsinstitut where FK_email = '$myusername'"; //Im Einsatz
+            $result = mysqli_query($conn, $query);
+            
+            echo "<h3>Benutzerprofil</h3>";
+            echo "\n\n<table>"
+            ."<tr><td>Name</td><td><input type='text' name='name[0]' value='' /></td></tr>"
+            ."<tr><td>Strasse</td><td><input type='text' name='strasse[0]' value='' /></td></tr>"
+            ."<tr><td>PLZ</td><td><input type='text' name='plz[0]' value='' /></td></tr>"
+            ."<tr><td>Ort</td><td><input type='text' name='ort[0]' value='' /></td></tr>"
+            ."<tr><td>Email</td><td><input type='text' name='email[0]' value='' /></td></tr>"
+            ."<tr><td><input type='submit' name'btnSubmit' value='Speichern'></td><td><input type='reset' name'btnReset' value='Reset'></td></tr>"
+            ."</table>";
+            
+            while($row= mysqli_fetch_array($result)){
+                echo "<tr><td>Name</td><td><input type='text' name='name[0]' value='".$result['Name']."' /></td></tr>"
+            ."<tr><td>Strasse</td><td><input type='text' name='strasse[0]' value='".$result['Strasse']."' /></td></tr>"
+            ."<tr><td>PLZ</td><td><input type='text' name='plz[0]' value='".$result['PLZ']."' /></td></tr>"
+            ."<tr><td>Ort</td><td><input type='text' name='ort[0]' value='".$result['Ort']."' /></td></tr>"
+            ."<tr><td>Email</td><td><input type='text' name='email[0]' value='".$result['FK_email']."' /></td></tr>";
+            }
         ?>
-        <h3>Benutzerprofil</h3>
-        <table>
-        <form name="record" action="userprofile.php">
-            <tr><td>Name</td><td><input type="text" name="name" value="" /></td></tr>
-            <tr><td>Strasse</td><td><input type="text" name="strasse" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
-            <tr><td>PLZ</td><td><input type="text" name="plz" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
-            <tr><td>Ort</td><td><input type="text" name="ort" value="" /></td><td><a href='javascript:edit($id);'><img src="../images/edit.png" alt="edit"></a></td></tr>
-            <tr><td><input type="submit" value="Speichern" name="save" /></td>
-            <td><input type="reset" value="Zurücksetzen" name="save" /></td></tr>
-        </form>
-        </table>
+        
     </body>
 </html>
