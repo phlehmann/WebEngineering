172e3f83a04c75aa8f040bb2805a2717e1c75e1f
Rene <Rene@DESKTOP-SA6NH8V.edu.ds.fhnw.ch>
2018-11-02 15:11:56 +0100

    Merge origin/master
    Conflicts:
    	index.php

---
+++ index2.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Home - Brand</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+        <link rel="stylesheet" href="assets/css/smoothproducts.css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+            <div class="container"><a class="navbar-brand logo" href="#"><img src="assets/img/Logo2.png" id="logo" style="width: 180px;height: 65px;"></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Meine Kurse</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="features.html">Benutzerprofil</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="pricing.html">Abmelden</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="about-us.html">de</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="about-us.html">EN</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="page landing-page">
+            <section class="clean-block clean-hero" style="background-image: url(&quot;assets/img/classroom.jpg&quot;);color: rgba(9,162,255,0.63);height: 600px;background-size: cover;background-repeat: no-repeat;min-height: 300px;">
+                <div id="course" class="text">
+                    <h2 style="font-weight: bold;">Kurse suchen</h2>
+                    <div class="row">
+                        <div class="col" style="min-width: auto;"><input type="text" style="width: 400px;height: 40px;padding: 0px;margin-top: 0px;margin-bottom: 20px;margin-right: 6px;margin-left: 8px;min-width: 300px;"></div>
+                        <div class="col"><button class="btn btn-primary" type="button" style="width: 180px;height: 40px;background-position: center;background-color: rgb(255,0,0);font-weight: bold;margin: 0;background-size: contain;">Suchen</button></div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="dropdown"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color: rgb(255,255,255);color: rgb(0,0,0);font-weight: normal;width: 180px;height: 40px;min-width: 150px;">Fachbereich &nbsp;</button>
+                                <div
+                                    class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="dropdown"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color: rgb(255,255,255);color: rgb(0,0,0);font-weight: normal;width: 180px;min-width: 150px;">Raum &nbsp;</button>
+                                <div
+                                    class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="dropdown"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color: rgb(255,255,255);color: rgb(0,0,0);font-weight: normal;width: 180px;height: 40px;min-width: 150px;">Kurs Typ &nbsp;</button>
+                                <div
+                                    class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="page-footer dark" style="height: 220px;">
+            <footer class="page-footer dark">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3" style="min-width: 240px;">
+                            <h5>Kontakt</h5>
+                            <p style="color: rgb(251,251,251);">Swissedu AG<br>Wängirain 53<br>8704 Herrliberg<br>info@swissedu.ch</p>
+                        </div>
+                        <div class="col-sm-3">
+                            <h5>Leistungen&nbsp;</h5>
+                            <ul>
+                                <li><a href="#"></a></li>
+                                <li><a href="#">Angebot</a></li>
+                                <li><a href="#">über uns</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-3" style="min-width: 240px;">
+                            <h5>Preise</h5>
+                            <p style="color: rgb(251,251,251);">3 &nbsp;Monate &nbsp; &nbsp;60 CHF<br>6 &nbsp;Monate &nbsp;100 CHF<br>12 Monate &nbsp;160 CHF</p>
+                        </div>
+                        <div class="col-sm-3" style="min-width: 240px;">
+                            <h5>Legal</h5>
+                            <ul>
+                                <li><a href="#" style="min-width: 160px;">Terms of Service</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright" style="margin-top: 0px;">
+                    <p>© 2018 Copyright Text</p>
+                </div>
+            </footer>
+        </footer>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+        <script src="assets/js/smoothproducts.min.js"></script>
+        <script src="assets/js/theme.js"></script>
+    </body>
+
+</html>
